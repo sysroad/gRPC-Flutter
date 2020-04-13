2d90010093457fcd47b591e041b599478ab9a2fd
byungil <sysroad@gmail.com>
2020-04-13 10:11:20 +0900

    프로젝트 생성, gitignore 추가

---
+++ HelloApp/android/app/src/main/kotlin/com/sampleapp/HelloApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sampleapp.HelloApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ HelloService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
